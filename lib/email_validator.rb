d859243d7be525d51d4294644331ba548b06ecc7
nyaago <nyaago69@me.com>
2011-02-18 09:55:05 +0900

    emailチェックの正規表現修正(キャプチャーグループ -> 非キャプチャーグループ)

--- lib/email_validator.rb
+++ lib/email_validator.rb
@@ -4,8 +4,8 @@ class EmailValidator < ActiveModel::EachValidator
  
   def validate_each(record, attribute, value)
 
-    regexp = Regexp.new('^([^@.<>\)\(\[\]]{1}[^@<>\)\(\[\]]+[^@.<>\)\(\[\]]{1})@' + 
-    "([a-zA-Z]{1}[-a-zA-Z0-9]+\.)+[a-zA-Z]{1}[-a-zA-Z0-9]+")
+    regexp = Regexp.new('^(?:[^@.<>\)\(\[\]]{1}[^@<>\)\(\[\]]+[^@.<>\)\(\[\]]{1})@' + 
+    "(?:[a-zA-Z]{1}[-a-zA-Z0-9]+\.)+[a-zA-Z]{1}[-a-zA-Z0-9]+$")
     unless regexp.match(value)
       record.errors[attribute] << I18n.t(:invalid_email_address, :scope => [:errors, :messages])
     end
