f8204fdc7c53c1a20d2b851f1bda31e115daf5bd
nyaago <nyaago69@me.com>
2011-02-22 00:28:15 +0900

    memoテキスト、作業用スクリプト更新

--- tool/test_site_and_user.rb
+++ tool/test_site_and_user.rb
@@ -1,4 +1,4 @@
-site = Site.create(:name => 'moomin', :title => 'moomin')
+site = Site.create(:name => 'moomin', :title => 'moomin', :email => 'nyaago69@gmail.com')
 user = User.new(:login => 'moomin', :email => 'nyaago69@gmail.com')
 user.password = user.password_confirmation = 'moomin'
 user.site = site
