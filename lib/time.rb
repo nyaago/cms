ae19cf6483c0f9293e683124dffe117e108138ea
nyaago <nyaago69@me.com>
2011-02-21 21:25:33 +0900

    環境依存で ?Time.utc がout of range エラーになるのでpatchあて

--- lib/time.rb
+++ lib/time.rb
@@ -5,12 +5,22 @@ class Time
   class << self
 
     
-    # utc メソッドのpatch
+    # utc メソッドのp
     alias_method :utc_without_trim, :utc 
   
     def utc_with_trim(*args)
     
-      if args.length >= 6
+      args.each do |v|
+        p "utc args - #{v}"
+      end
+    
+      if args.length >= 3 && args[4] >= 24
+        args[3] = 0
+      end
+      if args.length >= 5 && args[4] >= 60
+        args[4] = 0
+      end
+      if args.length >= 6 && args[5] >= 60
         args[5] = 0
       end
       utc_without_trim(*args)
