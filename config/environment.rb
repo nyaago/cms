1e5e53e21063b98609bb8ee0655e93fc8ddd4f38
nyaago <nyaago69@me.com>
2011-02-21 21:20:36 +0900

    環境依存で ?Time.utc がout of range エラーになるのでpatchあて

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Cms::Application.initialize!
+
+
+
+ret = require File.expand_path('../../lib/time', __FILE__)
