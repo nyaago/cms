6c02f7eeffcf40317089c4f6ba42f69973d0af06
nyaago <nyaago69@me.com>
2011-02-22 12:35:37 +0900

    layout, html template, ファイル構成などの文書追加 テストアカウントのスクリプト更新

--- tool/test_site_and_user.rb
+++ tool/test_site_and_user.rb
@@ -3,3 +3,9 @@ user = User.new(:login => 'moomin', :email => 'nyaago69@gmail.com')
 user.password = user.password_confirmation = 'moomin'
 user.site = site
 user.save!
+
+site = Site.create(:name => 'wasabi', :title => 'wasabi', :email => 'miki@wasabi3.com')
+user = User.new(:login => 'wasabi', :email => 'miki@wasabi3.com')
+user.password = user.password_confirmation = 'wasabi'
+user.site = site
+user.save!
