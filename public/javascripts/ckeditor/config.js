700fd1a2cebd68e7b28716d5046591596329f08b
nyaago <nyaago69@me.com>
2011-02-23 18:20:18 +0900

    記事への画像挿入,とりあえず一通り実装

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -3,23 +3,31 @@ Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
+CKEDITOR.config.extraPlugins = 'imageSelector';
+
 CKEDITOR.config.toolbar =
 [
 ['Source','-','Save','Preview'],
 ['Cut','Copy','Paste','PasteText','PasteFromWord'],
- ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+ ['Undo','Redo','-','Find','Replace','-','SelectAll'],
 
 '/',
 ['Bold','Italic','Underline','Strike'],
 ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
 ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
 ['Link','Unlink','Anchor'],
-['Image','Flash','Table','HorizontalRule','SpecialChar'],
+['Image','Flash','Table','HorizontalRule','SpecialChar','ImageSelector'],
 '/',
 ['Format','Font','FontSize'],
 ['TextColor','BGColor'],
 ['Maximize', 'ShowBlocks','-','About'],
 ];
+
+
+
+
+
+
 
 
 
