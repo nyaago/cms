e692435d9a31195267e822ef2458ce182960c80c
nyaago <nyaago69@me.com>
2011-04-14 16:08:04 +0900

    validationクラスのコメント追加

--- lib/domain_validator.rb
+++ lib/domain_validator.rb
@@ -1,5 +1,6 @@
 # = DomainValidator
 # Domain名のvalidator
+# ラテン文字・数字・“-”（先頭はラテン文字または数字）をdotで区切ったもののみ有効
 class DomainValidator < ActiveModel::EachValidator
  
   def validate_each(record, attribute, value)
--- lib/email_validator.rb
+++ lib/email_validator.rb
@@ -1,5 +1,13 @@
 # = Validator::Email
 # Email アドレスのvalidator
+# == ローカル部で可能な文字
+# * 大小のラテン文字
+# * 全角日本語
+# * 数字
+# * ! # $ % & ' * + - / = ? ^ _ ` { | } ~
+# * .  ただし、先頭と末尾以外で使用可能。　[2個以上連続してはならない]については未検証
+# == ドメイン部で可能な文字
+# ラテン文字・数字・“-”（先頭はラテン文字または数字）
 class EmailValidator < ActiveModel::EachValidator
  
   def validate_each(record, attribute, value)
